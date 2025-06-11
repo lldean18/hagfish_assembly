7c04b84a802dfc51fcf2aa72ec8c072d1796200d
Laura Dean <lldean18@gmail.com>
2025-06-11 13:57:08 +0100

    updated script to install and use basespace to download the ONT raw data

--- 01_download_data.bash
+++ 01_download_data.bash
@@ -12,13 +12,36 @@
 #SBATCH --time=10:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
+
 # set variables
 wkdir=/gpfs01/home/mbzlld/data/hagfish
-
 # move to working directory
 cd $wkdir
 
-# Download data from basespace with wget
 
+# install and load software
+source $HOME/.bash_profile
+#conda create --name basespace hcc::basespace-cli
+conda activate basespace
+# authenticate my basespace account
+#bs authenticate
+# then follow the link it gives you to log in
+# get the list of what's in my project
+#bs  content project --name S-25-0583_ONT
+#+-------------+-----------------------------+
+#|     Id      |          FilePath           |
+#+-------------+-----------------------------+
+#| 40757060038 | pcr-testis-gDNA-runs.tar    |
+#| 40750692865 | native-testis-gDNA-runs.tar |
+#+-------------+-----------------------------+
+
+# download the data
+bs download project --name S-25-0583_ONT --output raw_data
+
+
+
+
+# deactivate software
+conda deactivate
 
 
