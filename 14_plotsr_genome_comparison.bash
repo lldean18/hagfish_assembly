87a27af74716ee945d8e9cfef41becda60a13c61
Laura Dean <lldean18@gmail.com>
2025-07-03 16:47:29 +0100

    trying to get an alignment input for syri that will work. Trying with mummer

--- 14_plotsr_genome_comparison.bash
+++ 14_plotsr_genome_comparison.bash
@@ -7,9 +7,9 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=50g
-#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=70g
+#SBATCH --time=24:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
@@ -17,83 +17,89 @@
 wkdir=/gpfs01/home/mbzlld/data/hagfish/plotsr
 reference=/gpfs01/home/mbzlld/data/hagfish/Eptatretus_burgeri_ref/FYBX03_scaffs_only.fasta
 asm1=/gpfs01/home/mbzlld/data/hagfish/flye_1/assembly_ragtag/ragtag.scaffold_chrs_only.fasta
-# set mapping variable (select level based on estimated sequence divergence)
-# for reference asm5/asm10/asm20 = 0.1%/1%/5% sequence divergence
-#asm=asm5 # 0.1% sequence divergence
-asm=asm10 # 1% sequence divergence
-#asm=asm20 # 5% sequence divergence
 
 # load software
 source $HOME/.bash_profile
 module load samtools-uoneasy/1.18-GCC-12.3.0
 
 
-############################################
-##### map our assembly to the reference ####
-############################################
-#
-#echo "Mapping our assembly to the reference..."
+cd $wkdir
+
+###########################################
+#### map our assembly to the reference ####
+###########################################
+
+echo "Mapping our assembly to the reference..."
+
+
 #conda activate minimap2
-#
-## align the genomes
-#
-## for reference asm5/asm10/asm20 = 0.1%/1%/5% sequence divergence
+# for reference asm5/asm10/asm20 = 0.1%/1%/5% sequence divergence
 #minimap2 \
-#-ax $asm \
-#-t 16 \
+#-ax asm5 \
+#-t 1 \
 #--eqx $asm1 $reference \
-#-o $wkdir/$(basename ${asm1%.*})_$asm.sam
-##samtools sort $wkdir/tmp.sam \
-##-o $wkdir/$(basename ${asm1%.*})_$asm.bam
-##rm $wkdir/tmp.sam
+#-o $wkdir/$(basename ${asm1%.*}).sam
 #conda deactivate
-#
-## index the bam file
-##samtools index -bc $wkdir/$(basename ${asm1%.*})_$asm.bam
-#
+#module unload samtools-uoneasy/1.18-GCC-12.3.0
+
+
+# Trying with MuMmer instead because syri won't work with the sam or bam output by minimap2
+#conda create --name mummer bioconda::mummer -y
+conda activate mummer
+nucmer --maxmatch -c 100 -b 500 -l 50 $reference $asm1
+delta-filter -m -i 90 -l 100 out.delta > out.filtered.delta
+show-coords -THrd out.filtered.delta > out.filtered.coordsi
+conda deactivate
+
+conda activate syri
+syri -c out.filtered.coords -d out.filtered.delta -r $reference -q $asm1
+conda deactivate
+
+
+
 ## write the names of the assemblies to a file for use by plotsr
 #echo -e ""$asm1"\tEptatretus_stoutii
 #"$reference"\tEptatretus_burgeri" > $wkdir/$(basename ${asm1%.*})_plotsr_assemblies_list.txt
 #
-#module unload samtools-uoneasy/1.18-GCC-12.3.0
 #echo -e "Done\n\n"
 
 
-###############################################################
-#### Identify structural rearrangements between assemblies ####
-###############################################################
-
-echo "identifying structural rearrangements between assemblies with syri..."
-# create your syri environment
-#conda create -y --name syri -c bioconda -c conda-forge -c anaconda python=3.8 syri
-conda activate syri
-
-# Run syri to find structural rearrangements between your assemblies
-syri \
--c $wkdir/$(basename ${asm1%.*})_$asm.sam \
--r $reference \
--q $asm1 \
--F S \
---dir $wkdir \
---prefix $(basename ${asm1%.*})_${asm}_syri
-
-conda deactivate
-echo -e "Done\n\n"
+################################################################
+##### Identify structural rearrangements between assemblies ####
+################################################################
+#
+#echo "identifying structural rearrangements between assemblies with syri..."
+## create your syri environment
+##conda create -y --name syri -c bioconda -c conda-forge -c anaconda python=3.8 syri
+#conda activate syri
+#
+## Run syri to find structural rearrangements between your assemblies
+#syri \
+#-c $wkdir/$(basename ${asm1%.*}).sam \
+#-r $reference \
+#-q $asm1 \
+#-F S \
+#-k \
+#--dir $wkdir \
+#--prefix $(basename ${asm1%.*})_syri
+#
+#conda deactivate
+#echo -e "Done\n\n"
 
 ############################
 #### create plotsr plot ####
 ############################
 
-echo "plotting structural rearrangements with plotsr..."
-conda activate plotsr
-
-plotsr \
---sr $(basename ${asm1%.*})_${asm}_syri.out \
---genomes $wkdir/$(basename ${asm1%.*})_plotsr_assemblies_list.txt \
--o $wkdir/$(basename ${asm1%.*})_${asm}_plot.png
-
-conda deactivate
-echo "Done"
+#echo "plotting structural rearrangements with plotsr..."
+#conda activate plotsr
+#
+#plotsr \
+#--sr $(basename ${asm1%.*})_${asm}_syri.out \
+#--genomes $wkdir/$(basename ${asm1%.*})_plotsr_assemblies_list.txt \
+#-o $wkdir/$(basename ${asm1%.*})_${asm}_plot.png
+#
+#conda deactivate
+#echo "Done"
 
 
 
