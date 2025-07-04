0ac747819e617ec8a52bf8d4b61f8101c90101c7
Laura Dean <lldean18@gmail.com>
2025-07-04 12:20:41 +0100

    still out of memory so pushed to the hmemq

--- 14_plotsr_genome_comparison.bash
+++ 14_plotsr_genome_comparison.bash
@@ -4,11 +4,11 @@
 # script designed for running on the UoN HPC Ada
 
 #SBATCH --job-name=plotsr
-#SBATCH --partition=defq
+#SBATCH --partition=hmemq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=350g
+#SBATCH --mem=1495g
 #SBATCH --time=48:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
