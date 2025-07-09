d6d0506df1f14fcad2e6b6c60a101c8c445cd0bb
Laura Dean <lldean18@gmail.com>
2025-07-09 11:48:31 +0100

    ran out of time in hmemq so increasing time and mem to max

--- 14_plotsr_genome_comparison.bash
+++ 14_plotsr_genome_comparison.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1495g
-#SBATCH --time=48:00:00
+#SBATCH --time=168:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
